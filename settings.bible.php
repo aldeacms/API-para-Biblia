064a758754f60210f443a517bbbbac20a9ad0e82
Daniel <daniel.aldea@lvaindices.com>
2012-03-07 15:44:01 -0300

    Variables de configuración

---
+++ settings.bible.php
@@ -0,0 +1,13 @@
+<?php 
+/***********************************
+* CONFIGURACION 
+***********************************/
+
+var $dbhost = '';
+var $dbuser = '';
+var $dbpass = '';
+var $dbname = 'i';
+var $dbprefix = 'bible_';
+var $bible = 1; //rv60
+
+?>
